946cb2d9719233fa9abbdac71da8476ecaad0660
adb-sh <git@adb.sh>
2024-09-17 19:07:26 +0200

    Set esbuild target to esnext

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  // optimizeDeps: {
-  //   disabled: true,
-  // },
-  // plugins: [
-  //   solid({
-  //     ssr: true,
-  //   }),
-  // ],
-  // server: {
-  //   port: 3001,
-  //   host: "0.0.0.0",
+  server: {
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+  },
   // },
 });
